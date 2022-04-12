053ae1e91398c28d248037b48e01cf43883d011c
jarenreber <jaren.c.reber@gmail.com>
2022-04-12 15:49:34 -0600

    First commit

---
+++ server/controller.js
@@ -0,0 +1,42 @@
+let houses = require('./db.json')
+let globalID = 4
+
+module.exports = {
+    getHouses: (req, res) => {
+        res.status(200).send(houses)
+    },
+    createHouse: (req, res) => {
+        const { address, price, imageURL} = req.body
+        let newHouse = {
+            id: globalID,
+            address,
+            price,
+            imageURL
+        }
+        houses.push(newHouse)
+        globalID++;
+        res.status(200).send(houses)
+
+    },
+    updateHouse: (req, res) => {
+        const {id} = req.params
+        const {type} = req.body // ask in queue if we don't figure out.
+        let index = houses.findIndex(elem => elem.id === +id)
+
+        if (type === 'minus') {
+            houses[index].price -= 10000
+            res.status(200).send(houses)
+        } else if (type === 'plus') {
+            houses[index].price += 10000
+            res.status(200).send(houses)
+        } else {
+            res.status(400).send('Something went wrong.')
+        }
+
+    },
+    deleteHouse: (req, res) => {
+        let index = houses.findIndex(elem => elem.id === +req.params.id)
+        houses.splice(index, 1)
+        res.status(200).send(houses)
+    }
+}
